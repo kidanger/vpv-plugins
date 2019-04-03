39fcbf92049801c20a9dbddb0d748b69a769ea95
Anger Jérémy <angerj.dev@gmail.com>
2019-04-03 19:47:04 +0200

    add a plugin for often used terminal commands

---
+++ kidanger/recentcmd.lua
@@ -0,0 +1,54 @@
+
+local path = os.getenv('HOME') .. '/.vpv'
+local file = path .. '/cmds'
+
+local p = {}
+local list = {}
+local has = {}
+local currentcmd = ''
+
+function p.init()
+    local f = io.open(file, 'r')
+    if not f then return end
+    for l in f:lines() do
+        print(l)
+        table.insert(list, l)
+        has[l] = true
+    end
+end
+
+local function write()
+    local f = assert(io.open(file, 'w'))
+    f:write(table.concat(list, '\n'))
+    f:close()
+end
+
+function p.gui()
+    text 'List of commands'
+    for i, c in ipairs(list) do
+        if button(c) then
+            set_terminal_command(c)
+        end
+        sameline()
+        if button('remove##'..c) then
+            table.remove(list, i)
+            has[c] = false
+            write()
+            print('removed', c)
+        end
+    end
+
+    text('current commandline:')
+    sameline()
+    local c = get_terminal_command()
+    text(c)
+    if #c ~= 0 and not has[c] and (sameline() or button 'add') then
+        table.insert(list, c)
+        has[c] = true
+        write()
+        print('add ', c)
+    end
+end
+
+return p
+
