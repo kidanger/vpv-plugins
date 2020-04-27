a2290846e42860be307475ffed3207821e2da8c9
Anger Jérémy <angerj.dev@gmail.com>
2020-04-27 21:35:51 +0200

    add seq2gif: export a gif of a sequence

---
+++ python/seq2gif.py
@@ -0,0 +1,69 @@
+from api import *
+
+import os
+
+seq = None
+win = None
+oldconfigscreenshot = None
+FILES = '/tmp/screenshot_%05d.png'
+ALLFILES = '/tmp/screenshot_*.png'
+OUTPUT = '/tmp/out.gif'
+
+frames = []
+hasframe = {}
+
+def on_tick():
+    global win, seq, oldconfigscreenshot, frames, hasframe
+
+    if not seq and is_key_pressed(';'):
+        win = get_focused_window()
+        seq = win.current_sequence
+        player = seq.player
+        player.frame = 1
+        frames = list(range(player.current_min_frame, player.current_max_frame+1))
+        hasframe = {}
+        player.opened = True
+        player.playing = False
+
+        oldconfigscreenshot = get_config('SCREENSHOT')
+        set_config('SCREENSHOT', FILES)
+        os.system('rm {}'.format(ALLFILES))
+
+        print('Configure the FPS, bounds and bounce and close the player (click on the cross) to create a gif of the sequence.')
+        print('Press ; again to abort.')
+        return
+
+    if not seq:
+        return
+
+    if is_key_pressed(';'):
+        print('Gif aborted.')
+        seq = None
+        return
+
+    if seq.player.opened:
+        return
+
+    seq.player.frame += 1
+
+    if seq.player.frame not in hasframe:
+        win.take_screenshot()
+        hasframe[seq.player.frame] = True
+        return
+
+    hasall = True
+    for f in frames:
+        if f not in hasframe:
+            hasall = False
+            break
+
+    if hasall and seq.player.frame == frames[1]:
+        set_config('SCREENSHOT', oldconfigscreenshot)
+        files = [FILES % i for i in range(1, len(frames)+1)]
+        if seq.player.bouncy:
+            files += files[-2:0:-1]
+        cmd = 'echo "Converting to gif..." && convert -delay 1x{} {} {} && echo "All done!" &'.format(seq.player.fps, ' '.join(files), OUTPUT)
+        print(cmd)
+        os.system(cmd)
+        seq = None
+
