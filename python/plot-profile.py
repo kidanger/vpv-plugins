857a0141551467f488cd580de44a93d108fd645e
Anger Jérémy <angerj.dev@gmail.com>
2020-04-29 22:29:35 +0200

    plot profile: use the scriptSVG feature instead of writing to files

--- python/plot-profile.py
+++ python/plot-profile.py
@@ -30,7 +30,7 @@ def bresenham(x0, y0, x1, y1):
 H = 150
 W = 300
 
-def trace(image, start, end):
+def trace(seq, image, start, end):
     import numpy as np
     def dist(p):
         d1 = np.hypot(p[0]-start[0], p[1]-start[1])
@@ -46,34 +46,32 @@ def trace(image, start, end):
     ma = np.max(samples)
     mi = np.min(samples)
     def plot(samples):
+        # TODO: normalize using the colormap
         s = [(dist(p)*W, (1-(s-mi)/(ma-mi))*H) for p, s in zip(pos, samples)]
         return [(x,y) for x, y in s]
 
-    # TODO add-svg-from-code: instead of a file, give the svg directly to vpv
-    with open('/tmp/profile-plot.svg', 'w') as f:
-        f.write('<svg width="1" height="1">')
-        # draw the segment
+    code = '<svg width="1" height="1">'
+    # draw the segment
 # <circle r='1' cx='{start[0]+0.5}' cy='{start[1]+0.5}' fill='#eeeeee' fill-opacity='0.5'></rect>
-        f.write(f'''
+    code += f'''
 <polyline stroke="#11ee11" stroke-width="-2" points="{start[0]+0.5},{start[1]+0.5} {end[0]+0.5},{end[1]+0.5}"/>
 <polyline stroke="#111111" stroke-width="-1" points="{start[0]+0.5},{start[1]+0.5} {end[0]+0.5},{end[1]+0.5}"/>
-''')
-        # draw the rectangle for the plot
-        f.write(f'''
+'''
+    # draw the rectangle for the plot
+    code += f'''
 <rect display='absolute' width="{W}" height='{H}' x='0' y='0' fill='#eeeeee' fill-opacity='0.85'></rect>
-''')
-        # draw the plot (max 3 channels)
-        for i, c in zip(range(samples.shape[1]), ('#dd3333', '#33dd33', '#3333dd')):
-            f.write(f'''
+'''
+    # draw the plot (max 3 channels)
+    for i, c in zip(range(samples.shape[1]), ('#dd3333', '#33dd33', '#3333dd')):
+        code += f'''
 <polyline display='absolute' stroke="{c}" stroke-width="1"
-    points="{' '.join(str(x)+','+str(y) for x, y in plot(samples[:,i]))}"/>
-''')
-        f.write('</svg>')
+points="{' '.join(str(x)+','+str(y) for x, y in plot(samples[:,i]))}"/>
+'''
+    code += '</svg>'
+    seq.put_script_svg('plot-profile', code)
 
-def clear():
-    with open('/tmp/profile-plot.svg', 'w') as f:
-        f.write('<svg width="1" height="1">')
-        f.write('</svg>')
+def clear(seq):
+    seq.put_script_svg('plot-profile')
 
 start = None
 end = None
@@ -84,6 +82,8 @@ def on_tick():
     window = get_focused_window()
     if not window or not window.current_sequence:
         return
+
+    seq = window.current_sequence
 
     global start, end, curimage, closed
 
@@ -106,13 +106,13 @@ def on_tick():
             start = None
             end = None
             closed = False
-            clear()
+            clear(seq)
 
     # TODO: support many sequences at the same time (require add-svg-from-code)
 
-    image = window.current_sequence.image
+    image = seq.image
     isanewimage = image and (image.id != curimage.id if curimage else True)
     if (isanewimage or changed) and start and end:
         curimage = image
-        trace(image, start, end)
+        trace(seq, image, start, end)
 
