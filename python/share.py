4d9199dceeaa4f5eb75db07851b442c62c60cb6c
Anger Jérémy <angerj.dev@gmail.com>
2020-11-16 20:26:33 +0100

    share: write some doc, and some fixes

--- python/share.py
+++ python/share.py
@@ -1,3 +1,32 @@
+"""
+Required:
+$ pip install rangehttpserver
+
+How it works:
+    - if the envvar VPV_PROXY_COMMAND is defined, launch it. it is expected to print a remote_url
+    - creates a temporary directory
+    - for each image in each sequence, creates a symlink to it in the temp directory
+    - creates a index.html file that contains the urls of the images (arranged in sequences)
+    - opens an http server to serve the images and the index.html (on port 3395)
+
+Without proxy:
+HOST:[python http server] <-> CLIENTS:[vpv gdal image loader (http)]
+
+With a simple ssh proxy that forwards the connections from a public port:
+HOST:[python http server] <-> PROXY:[ssh] <-> CLIENTS:[vpv gdal image loader (http)]
+This is useful if the HOST is behind a firewall. However, the connection from the proxy to the clients will be http (not secure).
+Example of VPV_PROXY_COMMAND:
+$ export VPV_PROXY_COMMAND="echo \"http://<server-ip>:8895\"; exec ssh -N -R :8895:localhost:PORT <server>"
+where 8895 is an example of public port on the server (and "GatewayPorts clientspecified" in the sshd config).
+
+With a ssh proxy and an https proxy on the same server:
+HOST:[python http server] <-> PROXY:[ssh] <-> PROXY:[https proxy] <-> CLIENTS:[vpv gdal image loader (https)]
+This is useful if the proxy is a public server, with an https proxy running.
+Example of VPV_PROXY_COMMAND:
+$ export VPV_PROXY_COMMAND="echo \"https://<server-ip>:3000\"; exec ssh -N -R :8895:localhost:PORT <server>"
+where 8895 is now a private port on the server, but 3000 corresponds to the https proxy (that will talk with the port 8895).
+"""
+
 from api import *
 
 import os
@@ -18,16 +47,18 @@ remote_url = None
 def create_server(port=3395):
     global thread, tmpdir, remote_url
 
-    if 'VPV_PROXY_COMMAND' not in os.environ:
-        raise RuntimeError('VPV_PROXY_COMMAND not in environnement variable')
-    cmd = os.environ['VPV_PROXY_COMMAND']
-    cmd = cmd.replace('PORT', str(port))
+    if 'VPV_PROXY_COMMAND' in os.environ:
+        cmd = os.environ['VPV_PROXY_COMMAND']
+        cmd = cmd.replace('PORT', str(port))
 
-    print('[INFO] running proxy command:', cmd)
-    proxy = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    atexit.register(os.kill, proxy.pid, signal.SIGTERM)
+        print('[INFO] running proxy command:', cmd)
+        proxy = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        atexit.register(os.kill, proxy.pid, signal.SIGTERM)
 
-    remote_url = proxy.stdout.readline().decode('utf-8').strip()
+        remote_url = proxy.stdout.readline().decode('utf-8').strip()
+    else:
+        print('[INFO] VPV_PROXY_COMMAND not in environnement, ignoring the proxy')
+        remote_url = f'http://localhost:{port}'
     tmpdir = tempfile.TemporaryDirectory()
 
     handler = functools.partial(RangeRequestHandler, directory=tmpdir.name)
@@ -56,9 +87,9 @@ def on_tick():
         with open(f'{tmpdir.name}/index.html', 'w') as f:
             f.write(' '.join(seqs))
 
-        print('Send the following command to others:')
+        print('Send the following command to users. They need to have the GDAL support for vpv.')
         print('-----------')
-        print(f'bash -c "VPVCMD=<(curl {remote_url}) vpv"')
+        print(f"bash -c 'VPVCMD=<(curl {remote_url}) vpv'")
         print('-----------')
 
 
